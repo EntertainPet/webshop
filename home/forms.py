8bf840997c070fb372a6d34a6aec2094975f074f
MIREGAR1 <migreggar@alum.us.es>
2025-11-15 23:32:43 +0100

    feat: Register form

--- home/forms.py
+++ home/forms.py
@@ -1,18 +1,21 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from .models import Cliente
+from django.contrib.auth import get_user_model
 
+
+User = get_user_model()
 class ClienteRegistrationForm(UserCreationForm):
-    email = forms.EmailField(required=False)
+    telefono = forms.CharField(
+        max_length=20, 
+        widget=forms.TextInput(attrs={'placeholder': '+34 600 123 456'})
+    )
 
-    class Meta:
-        model = Cliente
+    class Meta(UserCreationForm.Meta):
+        model = User
         fields = [
             "username", "email",
             "first_name", "last_name",
             "telefono", "direccion", "ciudad", "codigo_postal",
-            "password1", "password2",
+            "password", "password2",
         ]
 
-class ClienteLoginForm(AuthenticationForm):
-    pass
