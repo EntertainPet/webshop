fea0ab734241e1fe8c558eaf0704792c9210354e
OscarGomezGonzalezz <oscgomgon.business@gmail.com>
2025-11-24 13:34:22 +0100

    fix: sec warning

--- EntertainPet/settings.py
+++ EntertainPet/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#*dnr@#5voq^z+yun*)oyzqvt-)+2q2q53^l89yv*zx_@l)u!e'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
